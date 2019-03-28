2a07ccb9048e19b9247c707ce10ca141226fa7b4
Roy Scheffers <r.scheffers@gmail.com>
2019-03-28 16:29:22 +0100

    add project structure files

---
+++ frontend/pages/Sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = () => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
+
+export default Sell;
---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => (
+  <div>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
+);
+
+export default Home;
