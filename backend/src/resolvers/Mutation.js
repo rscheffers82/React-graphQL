18e2f4b549c0b39ba0aeb393137cbbdad6eb8f14
Roy Scheffers <r.scheffers@gmail.com>
2019-04-09 10:36:12 +0200

    Add item query and mutation logic

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,26 @@
+// Resolver file, logic for the mutations
+
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        const newDog = {
-            name: args.name
-        }
-        global.dogs.push(newDog);
-        return newDog;
+    // info contains the query from the front-end. This is needed to the backend and therefore added tot he createItem function.
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args,
+            }
+        }, info);
+
+        return item;
     }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     const newDog = {
+    //         name: args.name
+    //     }
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,16 @@
+const { forwardTo } = require('prisma-binding');
+
+// Resolver file, logic for the queries
+
 const Query = {
-    dogs(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        return global.dogs;
-    }
+    // When the query is exactly the same on the frontend as in the backend, then the query can be forwarded like below
+    // This is the case when no authentication or data manipulation is needed.
+    items: forwardTo('db'),
+    // The above is the same as the below items function.
+    async items(parent, args, ctx, info) {
+        const items = await ctx.db.query.items();
+        return items;
+    },
 };
 
 module.exports = Query;
