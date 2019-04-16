ab3bd537e33282ff881e7c475d7ece2bb526dceb
Roy Scheffers <r.scheffers@gmail.com>
2019-04-16 11:16:10 +0200

    Add upload functionality to createItem

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -7,6 +7,12 @@ import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
 
+/* Improvement:
+ *  - Disable the submit button until the file is uploaded
+      currently the user can submit while the file is still being uploaded.
+      The API will catch this and display an error, but the UX can be improved here.
+*/
+
 const CREATE_ITEM_MUTATION = gql`
     mutation CREATE_ITEM_MUTATION(
         $title: String!
@@ -31,7 +37,7 @@ class CreateItem extends Component {
     state = {
         title: 'Cool shoes',
         description: 'Some really awesome wanderers',
-        image: 'shoes.jpg',
+        image: undefined,
         largeImage: 'large-shoes.jpg',
         price: 2499,
     }
@@ -40,6 +46,29 @@ class CreateItem extends Component {
         const {name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: value });
+    }
+
+    uploadFile = async e => {
+        console.log();
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch(
+            'https://api.cloudinary.com/v1_1/Smartimg/image/upload',
+            {
+                method: 'POST',
+                body: data,
+            }
+        );
+        const file = await res.json();
+        console.log(file);
+
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        })
     }
 
     render() {
@@ -65,6 +94,19 @@ class CreateItem extends Component {
                             disabled={loading}
                             aria-busy={loading}
                         >
+                        {this.state.image && <img src={this.state.image} alt="Upload preview" />}
+                            <label htmlFor="file">
+                            File
+                            <input
+                                type="file"
+                                id="file"
+                                name="file"
+                                placeholder="Upload an image"
+                                required
+                                onChange={this.uploadFile}
+                            />
+                            </label>
+
                             <label htmlFor="title">
                             Title
                             <input
